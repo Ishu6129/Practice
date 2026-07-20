e30963015f51abac73a3f151159ec39f9274d9e1
Ishu6129 <ishuagrawal124356@gmail.com>
2026-07-20 22:58:47 +0530

    basic UI bot with inMemory

---
+++ langGraph/CX_ChatBots/basic_ui_inmemory/frontend.py
@@ -0,0 +1,31 @@
+
+from logging import config
+
+import streamlit as st
+from backend import chatbot
+from langchain_core.messages import HumanMessage
+
+thread_id=1
+CONFIG={"configurable":{"thread_id":thread_id}}
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"]=[]
+
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input=st.chat_input()
+
+if user_input:
+    st.session_state["message_history"].append({'role': 'user', 'content': user_input})
+    with st.chat_message("user"):
+        st.write(user_input)
+    
+    response=chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=CONFIG)
+
+    ai_response=response["messages"][-1].content
+
+    st.session_state["message_history"].append({'role': 'assistant', 'content': ai_response})
+    with st.chat_message("assistant"):
+        st.write(ai_response)
